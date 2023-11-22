42fe2c33a802b62ca2d80f594a3409d72b6455d7
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-11-22 12:39:42 -0300

    Criando estilos globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,17 @@
-import type { AppProps } from 'next/app' 
+import { Roboto } from 'next/font/google'
+import type { AppProps } from 'next/app'
+
+const roboto = Roboto({ //pertence ao tipo de fonte do site
+  weight: '400',
+  subsets: ['latin'],
+})
+
+import '../styles/global.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
