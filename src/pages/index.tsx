7e9c17036b1f7e7b167f19a0e6179e4b02ff52c8
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2023-12-19 13:33:28 -0300

    Criando pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,33 @@
+import Head from 'next/head'
+import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
     <>
-      <h1>Whereas</h1>
+      <Head>
+        <title>Apaixonado por tecnologia - sujeito programador</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText}>
+            <h1>Levando você ao próximo nível?</h1>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Cupiditate voluptas aliquid, labore delectus illum placeat quaerat
+              quis est? Harum expedita aperiam quasi fugiat atque! Nemo
+              consequatur sint perferendis facere iure?
+            </span>
+            <a>
+              <button>Começar agora!</button>
+            </a>
+          </section>
+          <img
+            src="/images/banner-conteudos.png"
+            alt="Conteúdos sujeito programador"
+          />
+        </div>
+      </main>
     </>
   )
 }
